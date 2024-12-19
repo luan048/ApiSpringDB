ec8fc54a65bd179369a4601238994deb931dc648
luan048 <luanmonteiropedrosa@gmail.com>
2024-12-19 16:02:20 -0300

    Create API

---
+++ ApiSpringDB/src/main/java/com/company/ApiSpringDB/ApiSpringDbApplication.java
@@ -0,0 +1,13 @@
+package com.company.ApiSpringDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSpringDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSpringDbApplication.class, args);
+	}
+
+}
---
+++ ApiSpringDB/src/test/java/com/company/ApiSpringDB/ApiSpringDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.ApiSpringDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSpringDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
